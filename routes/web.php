f3d9513d2b3e320f965bfaab5790eee82e29dc24
d-arsya <kamaluddin.arsyad17@gmail.com>
2024-10-16 10:22:47 +0700

    Change route with route resource

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function(){
     return redirect("/books");
 });
-Route::get('/books', [BookController::class, 'index'])->name("books.index");
-Route::get('/books/create', [BookController::class, 'create'])->name("books.create");
-Route::post('/books/create', [BookController::class, 'store'])->name("books.store");
-Route::delete('/books/{id}', [BookController::class, 'destroy'])->name("books.destroy");
-Route::get('/books/update/{id}', [BookController::class, 'edit'])->name("books.edit");
-Route::post('/books/update/{id}', [BookController::class, 'update'])->name("books.update");
+// Route::get('/books', [BookController::class, 'index'])->name("books.index");
+// Route::get('/books/create', [BookController::class, 'create'])->name("books.create");
+// Route::post('/books/create', [BookController::class, 'store'])->name("books.store");
+// Route::delete('/books/{id}', [BookController::class, 'destroy'])->name("books.destroy");
+// Route::get('/books/update/{id}', [BookController::class, 'edit'])->name("books.edit");
+// Route::post('/books/update/{id}', [BookController::class, 'update'])->name("books.update");
+Route::resource('books',BookController::class)->except('show');
